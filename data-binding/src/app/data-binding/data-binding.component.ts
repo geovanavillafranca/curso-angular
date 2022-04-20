95721682501308614800fcb0233b306a34589c02
Geovana  Villafranca <geovanavillafranca@hotmail.com>
2022-04-20 11:25:23 -0300

    aula 11 - Event binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -10,10 +10,27 @@ export class DataBindingComponent implements OnInit {
   url: string = 'http://loiane.com'
   urlImagem = 'https://cdn.leroymerlin.com.br/products/papel_de_parede_paisagem_lago_natureza_sala_painel_052pcp_1567477748_5fd8_600x600.jpg'
 
+  valorAtual: string = '';
+  valorSalvo: string = '';
+  isMouseOver: boolean = false;
   cursoAngular: boolean = true;
 
+  botaoCriado(){
+    alert('Botao clicado!');
+  }
+
+  onKeyUp(evento: KeyboardEvent){
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+  salvarValor(valor: any) {
+    this.valorSalvo = valor;
+  }
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
+  }
+
   getValor(){
-    return 1 
+    return 1
   }
 
   getCurtirCurso(){
